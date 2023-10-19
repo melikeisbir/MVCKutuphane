ca8e020e3c5569f77a06ae400922351350610445
Melike İsbir <melikeisbr@gmail.com>
2023-10-19 18:55:30 +0300

    Table List Add Category

--- MVCKutuphane/Controllers/KategoriController.cs
+++ MVCKutuphane/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
 
 namespace MVCKutuphane.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        kutuphanedbEntities1 db = new kutuphanedbEntities1();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
         }
     }
 }
